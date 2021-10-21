07a78f49568649a3231afd6f52ea1efbfddbc249
Izzy Engelbert <izzyengelbert@gmail.com>
2021-10-21 21:03:30 +0700

    Remove log

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const select = require('./select');
 
 module.exports = (query, type, inRegion) => {
-	console.log(query, type, inRegion)
 	const result = select[type](query, inRegion);
 	return result ? result : select.kelurahan(query, inRegion)
 };
